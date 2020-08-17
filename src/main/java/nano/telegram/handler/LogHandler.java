932108c7e35f7b0b13b809fda1986c3caaae4d18
cbdyzj <cbdyzj@jianzhao.org>
2020-08-17 11:23:47 +0800

    Amend

--- src/main/java/nano/telegram/handler/LogHandler.java
+++ src/main/java/nano/telegram/handler/LogHandler.java
@@ -3,14 +3,17 @@ package nano.telegram.handler;
 import lombok.extern.slf4j.Slf4j;
 import nano.support.Onion;
 import nano.telegram.BotContext;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class LogHandler implements Onion.Middleware<BotContext> {
 
     @Override
     public void via(BotContext context, Onion.Next next) throws Exception {
         var parameters = context.getParameters();
         log.info("parameters: {}", parameters);
+        // next
         next.next();
         var attributes = context.getAttributes();
         log.info("result: {}", attributes.get("result"));
