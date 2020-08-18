dda5a89ea318b27a18a7d0a372e2dfe6607f4cb5
cbdyzj <cbdyzj@jianzhao.org>
2020-08-18 10:15:15 +0800

    Amend

--- src/main/java/nano/security/AuthenticationException.java
+++ src/main/java/nano/security/AuthenticationException.java
@@ -1,14 +1,15 @@
 package nano.security;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * Reply HTTP status 403 and reason
  */
-public class AuthenticationException extends ResponseStatusException {
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class AuthenticationException extends RuntimeException {
 
-    public AuthenticationException(String reason) {
-        super(HttpStatus.FORBIDDEN, reason);
+    public AuthenticationException(String message) {
+        super(message);
     }
 }
