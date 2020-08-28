ca833124960b7b92ab6f546e41f2b1ff994dbf51
cbdyzj <cbdyzj@jianzhao.org>
2020-08-28 19:05:52 +0800

    Amend

--- src/main/java/nano/telegram/BotHandler.java
+++ src/main/java/nano/telegram/BotHandler.java
@@ -9,42 +9,32 @@ import nano.telegram.handler.text.BabelHandler;
 import nano.telegram.handler.text.EvalHandler;
 import nano.telegram.handler.text.FoolHandler;
 import nano.telegram.handler.text.WikiHandler;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
 public class BotHandler {
 
-    @NonNull
-    private final ExceptionHandler exceptionHandler;
-    @NonNull
-    private final LogHandler logHandler;
-
-    // text message
-
-    @NonNull
-    private final BabelHandler babelHandler;
-    @NonNull
-    private final WikiHandler wikiHandler;
-    @NonNull
-    private final FoolHandler foolHandler;
-    @NonNull
-    private final EvalHandler evalHandler;
-
     private final Onion<BotContext> onion = new Onion<>();
+
+    @NonNull
+    private final ApplicationContext applicationContext;
 
     @PostConstruct
     public void init() {
-        this.onion.use(this.exceptionHandler);
-        this.onion.use(this.logHandler);
+        var ctx = Objects.requireNonNull(this.applicationContext);
+        this.onion.use(ctx.getBean(ExceptionHandler.class));
+        this.onion.use(ctx.getBean(LogHandler.class));
         // text message
         var textMessageHandler = Onion.compose(
-                this.babelHandler,
-                this.wikiHandler,
-                this.evalHandler,
-                this.foolHandler
+                ctx.getBean(BabelHandler.class),
+                ctx.getBean(WikiHandler.class),
+                ctx.getBean(EvalHandler.class),
+                ctx.getBean(FoolHandler.class)
         );
         this.onion.use(textMessageHandler);
     }
@@ -52,4 +42,5 @@ public class BotHandler {
     public void handle(BotContext context) throws Exception {
         this.onion.handle(context);
     }
+
 }
