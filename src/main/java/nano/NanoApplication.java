940d0ab04f7fded5f2b8179866bb1c76f3d4a2b9
cbdyzj <cbdyzj@jianzhao.org>
2020-09-04 11:25:29 +0800

    Amend

--- src/main/java/nano/NanoApplication.java
+++ src/main/java/nano/NanoApplication.java
@@ -2,7 +2,6 @@ package nano;
 
 import nano.component.ConfigVars;
 import nano.security.AuthenticationInterceptor;
-import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
--- src/main/java/nano/controller/TelegramController.java
+++ src/main/java/nano/controller/TelegramController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-import static nano.security.AuthenticationInterceptor.TOKEN;
-
 /**
  * Handle telegram request
  *
