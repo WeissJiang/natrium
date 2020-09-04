3e8fcd7aaec19731dad2bb85a18d9a0f0b9b1647
cbdyzj <cbdyzj@jianzhao.org>
2020-09-04 17:55:09 +0800

    Amend

--- src/main/java/nano/security/repository/SessionRepository.java
+++ src/main/java/nano/security/repository/SessionRepository.java
@@ -3,8 +3,16 @@ package nano.security.repository;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import nano.security.entity.Session;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Map;
 
 @Repository
 @RequiredArgsConstructor
@@ -14,24 +22,74 @@ public class SessionRepository {
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
     public Integer createSessionAndReturnsPrimaryKey(Session session) {
-        return 0;
-//        var sql = """
-//                INSERT INTO nano_session (chat_id, user_id, attributes, creation_time, last_accessed_time)
-//                VALUES (:chatId, :userId, :attributes, :creationTime, :lastAccessedTime);
-//                """;
-//        var paramSource = new BeanPropertySqlParameterSource(session);
-//        var keyHolder = new GeneratedKeyHolder();
-//        this.jdbcTemplate.update(sql, paramSource, keyHolder);
-//        var generatedKey = keyHolder.getKey();
-//        Assert.notNull(generatedKey, "generatedKey");
-//        return generatedKey.intValue();
+        var sql = """
+                INSERT INTO nano_session (chat_id, user_id, attributes, creation_time, last_accessed_time)
+                VALUES (:chatId, :userId, :attributes, :creationTime, :lastAccessedTime);
+                """;
+        var paramSource = new BeanPropertySqlParameterSource(session);
+        var keyHolder = new GeneratedKeyHolder();
+        this.jdbcTemplate.update(sql, paramSource, keyHolder);
+        var generatedKey = keyHolder.getKey();
+        Assert.notNull(generatedKey, "generatedKey");
+        return generatedKey.intValue();
     }
 
     public Session querySession(Number chatId, Number userId) {
-        return new Session();
+        var sql = """
+                SELECT chat_id, user_id, attributes, creation_time, last_accessed_time
+                FROM nano_session
+                WHERE TRUE
+                  AND chat_id = :chatId
+                  AND user_id = :userId
+                LIMIT 1;
+                """;
+        var paramMap = Map.of(
+                "chatId", chatId,
+                "userId", userId
+        );
+        var rowMapper = new BeanPropertyRowMapper<>(Session.class);
+        var sessionList = this.jdbcTemplate.query(sql, paramMap, rowMapper);
+        if (CollectionUtils.isEmpty(sessionList)) {
+            return null;
+        }
+        return sessionList.get(0);
     }
 
     public void updateSession(Session session) {
+        var sql = """
+                UPDATE nano_session
+                SET attributes         = :attributes,
+                    creation_time      = :creationTime,
+                    last_accessed_time = :lastAccessedTime
+                WHERE id = :id;
+                """;
+        var paramSource = new BeanPropertySqlParameterSource(session);
+        this.jdbcTemplate.update(sql, paramSource);
     }
 
+    @Transactional
+    public Session upsertSession(Session session) {
+
+        var chatId = session.getChatId();
+        var userId = session.getUserId();
+
+        var current = this.querySession(chatId, userId);
+        var createNew = false;
+        if (current == null) {
+            current = new Session();
+            current.setCreationTime(session.getLastAccessedTime());
+            createNew = true;
+        }
+        current.setChatId(chatId);
+        current.setUserId(userId);
+        current.setLastAccessedTime(session.getCreationTime());
+        // persistence
+        if (createNew) {
+            var id = this.createSessionAndReturnsPrimaryKey(current);
+            current.setId(id);
+        } else {
+            this.updateSession(current);
+        }
+        return current;
+    }
 }
--- src/main/java/nano/telegram/handler/SessionInitializeHandler.java
+++ src/main/java/nano/telegram/handler/SessionInitializeHandler.java
@@ -2,6 +2,7 @@ package nano.telegram.handler;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import nano.security.entity.Session;
 import nano.security.repository.SessionRepository;
 import nano.support.Onion;
@@ -9,6 +10,7 @@ import nano.telegram.BotContext;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class SessionInitializeHandler implements Onion.Middleware<BotContext> {
@@ -18,40 +20,31 @@ public class SessionInitializeHandler implements Onion.Middleware<BotContext> {
 
     @Override
     public void via(BotContext context, Onion.Next next) throws Exception {
-        // build session
-        var session = this.buildSession(context);
-        context.setSession(session);
-        next.next();
+        try {
+            // build session
+            var session = this.buildSession(context);
+            context.setSession(session);
+        } catch (Exception ex) {
+            log.warn("build session failed: {}", ex.getMessage());
+        } finally {
+            next.next();
+        }
     }
 
     private Session buildSession(BotContext context) {
         var chatId = context.chatId();
         var userId = context.fromId();
         var date = context.date();
+
         Assert.notNull(chatId, "chatId");
         Assert.notNull(userId, "userId");
 
-        var session = this.sessionRepository.querySession(chatId, userId);
-        var createNew = false;
-        if (session == null) {
-            session = new Session();
-            session.setCreationTime(date);
-            createNew = true;
-        }
+        var session = new Session();
         session.setChatId(chatId);
         session.setUserId(userId);
         session.setLastAccessedTime(date);
-        // persistence
-        this.persistSession(session, createNew);
-        return session;
+
+        return this.sessionRepository.upsertSession(session);
     }
 
-    private void persistSession(Session session, boolean createNew) {
-        if (createNew) {
-            var id = this.sessionRepository.createSessionAndReturnsPrimaryKey(session);
-            session.setId(id);
-        } else {
-            this.sessionRepository.updateSession(session);
-        }
-    }
 }
