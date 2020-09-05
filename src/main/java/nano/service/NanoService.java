36bf3ef594cc64abb2db8d0770d4a3899860e96d
cbdyzj <cbdyzj@jianzhao.org>
2020-09-05 12:07:04 +0800

    Amend

--- src/main/java/nano/service/NanoService.java
+++ src/main/java/nano/service/NanoService.java
@@ -2,13 +2,16 @@ package nano.service;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.SingleColumnRowMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.unit.DataSize;
 
 import java.lang.management.ManagementFactory;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class NanoService {
@@ -21,31 +24,36 @@ public class NanoService {
         nano.append("Postgres: ").append(this.getPostgresVersion()).append("\n");
         for (String key : SYSTEM_PROPERTIES) {
             var property = System.getProperty(key);
-            nano.append(key).append(property).append("\n");
+            nano.append(key).append(" :").append(property).append("\n");
         }
         var memoryMXBean = ManagementFactory.getMemoryMXBean();
 
         var heapMemoryUsage = memoryMXBean.getHeapMemoryUsage();
-        nano.append("heapMemoryUsage.init: ").append(heapMemoryUsage.getInit()).append("\n");
-        nano.append("heapMemoryUsage.committed: ").append(heapMemoryUsage.getCommitted()).append("\n");
-        nano.append("heapMemoryUsage.max: ").append(heapMemoryUsage.getMax()).append("\n");
-        nano.append("heapMemoryUsage.used: ").append(heapMemoryUsage.getUsed()).append("\n");
+        nano.append("heapMemoryUsage.init: ").append(megabytes(heapMemoryUsage.getInit())).append("\n");
+        nano.append("heapMemoryUsage.committed: ").append(megabytes(heapMemoryUsage.getCommitted())).append("\n");
+        nano.append("heapMemoryUsage.max: ").append(megabytes(heapMemoryUsage.getMax())).append("\n");
+        nano.append("heapMemoryUsage.used: ").append(megabytes(heapMemoryUsage.getUsed())).append("\n");
 
         var nonHeapMemoryUsage = memoryMXBean.getNonHeapMemoryUsage();
-        nano.append("nonHeapMemoryUsage.init: ").append(nonHeapMemoryUsage.getInit()).append("\n");
-        nano.append("nonHeapMemoryUsage.committed: ").append(nonHeapMemoryUsage.getCommitted()).append("\n");
-        nano.append("nonHeapMemoryUsage.max: ").append(nonHeapMemoryUsage.getMax()).append("\n");
-        nano.append("nonHeapMemoryUsage.used: ").append(nonHeapMemoryUsage.getUsed()).append("\n");
+        nano.append("nonHeapMemoryUsage.init: ").append(megabytes(nonHeapMemoryUsage.getInit())).append("\n");
+        nano.append("nonHeapMemoryUsage.committed: ").append(megabytes(nonHeapMemoryUsage.getCommitted())).append("\n");
+        nano.append("nonHeapMemoryUsage.max: ").append(megabytes(nonHeapMemoryUsage.getMax())).append("\n");
+        nano.append("nonHeapMemoryUsage.used: ").append(megabytes(nonHeapMemoryUsage.getUsed())).append("\n");
 
         var objectPendingFinalizationCount = memoryMXBean.getObjectPendingFinalizationCount();
-        nano.append("objectPendingFinalizationCount").append(objectPendingFinalizationCount).append("\n");
+        nano.append("objectPendingFinalizationCount: ").append(objectPendingFinalizationCount).append("\n");
         return nano.toString();
     }
 
     public String getPostgresVersion() {
-        var mapper = new SingleColumnRowMapper<String>();
-        var version = this.jdbcTemplate.query("SELECT VERSION();", mapper);
-        return String.join(", ", version);
+        try {
+            var mapper = new SingleColumnRowMapper<String>();
+            var version = this.jdbcTemplate.query("SELECT VERSION();", mapper);
+            return String.join(", ", version);
+        } catch (Exception ex) {
+            log.warn(ex.getMessage());
+            return "?";
+        }
     }
 
     private static final List<String> SYSTEM_PROPERTIES = List.of(
@@ -73,5 +81,9 @@ public class NanoService {
             "java.vm.version",
             "java.class.version"
     );
+
+    private String megabytes(long bytes) {
+        return "%sMB".formatted(DataSize.ofBytes(bytes).toMegabytes());
+    }
 
 }
