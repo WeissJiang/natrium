9685cb59edf1d0f629a24cc265925cd2294468a2
cbdyzj <cbdyzj@jianzhao.org>
2020-09-07 23:28:13 +0800

    Amend

--- worker/src/main/java/nano/worker/consumer/NanoConsumer.java
+++ worker/src/main/java/nano/worker/consumer/NanoConsumer.java
@@ -1,7 +1,6 @@
 package nano.worker.consumer;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
