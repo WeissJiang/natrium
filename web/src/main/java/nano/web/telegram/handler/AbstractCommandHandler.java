d103f9fc9cf14525d1cfd07f44dbf1952966a957
cbdyzj <cbdyzj@jianzhao.org>
2020-09-07 09:37:14 +0800

    Amend

--- web/src/main/java/nano/web/telegram/handler/AbstractCommandHandler.java
+++ web/src/main/java/nano/web/telegram/handler/AbstractCommandHandler.java
@@ -2,25 +2,41 @@ package nano.web.telegram.handler;
 
 import nano.support.Onion;
 import nano.web.telegram.BotContext;
-import nano.web.telegram.BotUtils;
 import org.springframework.util.StringUtils;
 
 public abstract class AbstractCommandHandler implements Onion.Middleware<BotContext> {
 
     @Override
     public void via(BotContext context, Onion.Next next) throws Exception {
-        var tail = BotUtils.parseCommand(this.command(), context.text());
+        var text = context.text();
+
+        if (StringUtils.isEmpty(text)) {
+            next.next();
+            return;
+        }
+
+        var tail = this.parseCommand(text);
 
         if (StringUtils.hasText(tail)) {
             this.handle(context, tail);
-        } else {
-            var commands = context.commands();
-            if (!commands.isEmpty() && ("/" + this.command()).equals(commands.get(0))) {
-                context.sendMessage(this.help());
-            } else {
-                next.next();
-            }
+        } else if (text.startsWith("/" + this.command())) {
+            context.sendMessage(this.help());
+        }else {
+            next.next();
         }
+    }
+
+    public String parseCommand(String text) {
+        var command = this.command();
+        if (StringUtils.isEmpty(text)) {
+            return null;
+        }
+        var regex = "(?i)^/?" + command.trim() + "\\s";
+        var split = text.trim().split(regex);
+        if (split.length < 2) {
+            return null;
+        }
+        return split[1].trim();
     }
 
     protected abstract void handle(BotContext context, String tail);
