4ac3985e5851f69434318a329e44d09c74a359f9
cbdyzj <cbdyzj@jianzhao.org>
2020-09-11 23:21:36 +0800

    Amend

--- web/src/main/resources/static/pages/openjdk/openjdk.jsx
+++ web/src/main/resources/static/pages/openjdk/openjdk.jsx
@@ -1,6 +1,7 @@
 import { React } from '/deps.mjs'
+import { assertEquals } from '/modules/assertions.mjs'
 
-function VersionSelect(props) {
+function VersionSelect() {
     return (
         <select name="version" defaultValue="11">
             <option value="8">OpenJDK 8 (LTS)</option>
@@ -11,26 +12,28 @@ function VersionSelect(props) {
     )
 }
 
-function ImplSelect(props) {
+function ImplSelect() {
     return (
         <select name="openjdk_impl" defaultValue="hotspot">
             <option value="hotspot">hotspot</option>
-            <option value="j9">j9</option>
+            <option value="openj9">j9</option>
         </select>
     )
 }
 
-function OSSelect(props) {
+function OSSelect() {
     return (
         <select name="os" defaultValue="macos">
-            <option value="macos">macOS</option>
+            <option value="mac">macOS</option>
             <option value="linux">Linux</option>
             <option value="windows">Windows</option>
+            <option value="solaris">Solaris</option>
+            <option value="aix">AIX</option>
         </select>
     )
 }
 
-function ArchSelect(props) {
+function ArchSelect() {
     return (
         <select name="arch" defaultValue="x64">
             <option value="x64">x64</option>
@@ -38,7 +41,7 @@ function ArchSelect(props) {
     )
 }
 
-function TypeSelect(props) {
+function TypeSelect() {
     return (
         <select name="type" defaultValue="jdk">
             <option value="jdk">JDK</option>
@@ -47,41 +50,138 @@ function TypeSelect(props) {
     )
 }
 
+function InfoTable(props) {
+    const link = url => <a href={url}>{url}</a>
+    const b2mb = size => ((size | 0) / 1024 / 1024).toFixed(2) + 'MB'
+    return (
+        <table>
+            <tbody>
+            <tr>
+                <td>Release name:</td>
+                <td>{props.release_name}</td>
+            </tr>
+            <tr>
+                <td>Release link:</td>
+                <td>{link(props.release_link)}</td>
+            </tr>
+            <tr>
+                <td>Binary name:</td>
+                <td>{props.binary_name}</td>
+            </tr>
+            <tr>
+                <td>Binary link:</td>
+                <td>{link(props.binary_link)}</td>
+            </tr>
+            <tr>
+                <td>Checksum link:</td>
+                <td>{link(props.checksum_link)}</td>
+            </tr>
+            <tr>
+                <td>Binary size:</td>
+                <td>{b2mb(props.binary_size)}</td>
+            </tr>
+            <tr>
+                <td>Installer name:</td>
+                <td>{props.installer_name}</td>
+            </tr>
+            <tr>
+                <td>Installer link:</td>
+                <td>{link(props.installer_link)}</td>
+            </tr>
+            <tr>
+                <td>Installer checksum link:</td>
+                <td>{link(props.installer_checksum_link)}</td>
+            </tr>
+            <tr>
+                <td>Installer size:</td>
+                <td>{b2mb(props.installer_size)}</td>
+            </tr>
+            <tr>
+                <td>Updated at:</td>
+                <td>{props.updated_at}</td>
+            </tr>
+            </tbody>
+        </table>
+    )
+}
+
 /**
  * @see https://api.adoptopenjdk.net/README
  */
 function OpenJDK() {
-    function getUrl(ev) {
+
+    const [loading, setLoading] = React.useState(false)
+    const [info, setInfo] = React.useState(false)
+
+    async function getUrl(ev) {
         ev.preventDefault()
         const form = new FormData(ev.target)
         // get form data
-        console.log('type', form.get('type'))
-        console.log('version', form.get('version'))
-        console.log('arch', form.get('arch'))
-        console.log('os', form.get('os'))
-        console.log('openjdk_impl', form.get('openjdk_impl'))
+        const searchParams = new URLSearchParams({
+            release: 'latest',
+            openjdk_impl: form.get('openjdk_impl'),
+            os: form.get('os'),
+            arch: form.get('arch'),
+            type: form.get('type'),
+        })
+
+        try {
+            setLoading(true)
+            setInfo(false)
+            const response = await fetch(`https://api.adoptopenjdk.net/v2/info/releases/openjdk${form.get('version')}?${searchParams}`)
+            assertEquals(response.status, 200, `response is ${response.status}`)
+            const data = await response.json()
+            setInfo(data)
+            setLoading(false)
+        } catch (ex) {
+            alert(ex.message)
+            setInfo(false)
+            setLoading(false)
+        }
+
     }
 
     return (
         <div>
             <form onSubmit={getUrl}>
-                <div>
-                    Version : <VersionSelect/>
-                </div>
-                <div>
-                    JVM: <ImplSelect/>
-                </div>
-                <div>
-                    Operate System: <OSSelect/>
-                </div>
-                <div>
-                    Architecture: <ArchSelect/>
-                </div>
-                <div>
-                    Type: <TypeSelect/>
-                </div>
-                <button type="submit">Get Download URL</button>
+                <table>
+                    <tbody>
+                    <tr>
+                        <td>Version:</td>
+                        <td><VersionSelect/></td>
+                    </tr>
+                    <tr>
+                        <td>JVM:</td>
+                        <td><ImplSelect/></td>
+                    </tr>
+                    <tr>
+                        <td>Operate System:</td>
+                        <td><OSSelect/></td>
+                    </tr>
+                    <tr>
+                        <td>Architecture:</td>
+                        <td><ArchSelect/></td>
+                    </tr>
+                    <tr>
+                        <td>Type:</td>
+                        <td><TypeSelect/></td>
+                    </tr>
+                    <tr>
+                        <td/>
+                        <td>
+                            <button type="submit" disabled={loading}>Get JDK Info</button>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
             </form>
+            {(function () {
+                if (info) {
+                    return <InfoTable {...Object.assign({}, info, info.binaries[0])}/>
+                } else if (loading) {
+                    return <span>loading...</span>
+                }
+            })()}
         </div>
     )
 }
