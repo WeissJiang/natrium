7a0c85d8217831b5b78ba96acaad022f4daff5f8
cbdyzj <cbdyzj@jianzhao.org>
2020-09-11 19:04:41 +0800

    set devtools.add-properties: false

--- web/src/main/resources/static/app.jsx
+++
@@ -1,6 +0,0 @@
-import { React } from './deps.mjs'
-import { ReactDOM } from './deps.mjs'
-
-import Nano from './nano.jsx'
-
-ReactDOM.render(<Nano/>, document.getElementById('app'));
---
+++ web/src/main/resources/static/pages/openjdk/openjdk.jsx
@@ -0,0 +1,83 @@
+import { React } from '/deps.mjs'
+
+function VersionSelect(props) {
+    return (
+        <select name="version" defaultValue="11">
+            <option value="8">OpenJDK 8 (LTS)</option>
+            <option value="11">OpenJDK 11 (LTS)</option>
+            <option value="14">JDK 14</option>
+            <option value="15">JDK 15</option>
+        </select>
+    )
+}
+
+function ImplSelect(props) {
+    return (
+        <select name="openjdk_impl" defaultValue="hotspot">
+            <option value="hotspot">hotspot</option>
+            <option value="j9">j9</option>
+        </select>
+    )
+}
+
+function OSSelect(props) {
+    return (
+        <select name="os" defaultValue="macos">
+            <option value="macos">macOS</option>
+            <option value="linux">Linux</option>
+            <option value="windows">Windows</option>
+        </select>
+    )
+}
+
+function ArchSelect(props) {
+    return (
+        <select name="arch" defaultValue="x64">
+            <option value="x64">x64</option>
+        </select>
+    )
+}
+
+function TypeSelect(props) {
+    return (
+        <select name="type" defaultValue="jdk">
+            <option value="jdk">JDK</option>
+            <option value="jre">JRE</option>
+        </select>
+    )
+}
+
+/**
+ * @see https://api.adoptopenjdk.net/README
+ */
+function OpenJDK() {
+    function getUrl(ev) {
+        ev.preventDefault()
+        console.log(ev.target)
+    }
+
+    return (
+        <div>
+            <form onSubmit={getUrl}>
+                <div>
+                    Version : <VersionSelect/>
+                </div>
+                <div>
+                    JVM: <ImplSelect/>
+                </div>
+                <div>
+                    Operate System: <OSSelect/>
+                </div>
+                <div>
+                    Architecture: <ArchSelect/>
+                </div>
+                <div>
+                    Type: <TypeSelect/>
+                </div>
+                <button type="submit">Get Download URL</button>
+            </form>
+        </div>
+    )
+}
+
+export default OpenJDK
