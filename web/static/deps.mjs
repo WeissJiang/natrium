c8066826a2b78b08ac1f76f1b6d3da5729b73088
cbdyzj <cbdyzj@jianzhao.org>
2020-09-14 08:29:58 +0800

    Use skypack cdn

--- web/static/deps.mjs
+++ web/static/deps.mjs
@@ -1,3 +1,3 @@
 // React
-export { default as React } from 'https://jspm.dev/react@16.13.1'
-export { default as ReactDOM } from 'https://jspm.dev/react-dom@16.13.1'
+export { default as React } from 'https://cdn.skypack.dev/react@^16.13.1'
+export { default as ReactDOM } from 'https://cdn.skypack.dev/react-dom@^16.13.1'
