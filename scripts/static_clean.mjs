d3d42d653097310d86967bd5f303f489884b8dbc
cbdyzj <cbdyzj@jianzhao.org>
2020-09-14 14:06:50 +0800

    Amend

--- scripts/static_clean.mjs
+++ scripts/static_clean.mjs
@@ -1,1 +1,28 @@
 #!/usr/bin/env node
+import { fileURLToPath } from 'url'
+import { dirname, join as joinPath } from 'path'
+import { readdir, stat, unlink, rmdir } from 'fs/promises'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const distPath = joinPath(__dirname, '..', 'web/dist')
+
+async function removeDir(dirPath) {
+    const files = await readdir(dirPath)
+    for (const file of files) {
+        const filePath = joinPath(dirPath, file)
+        const fileStats = await stat(filePath)
+        if (fileStats.isDirectory()) {
+            await removeDir(filePath)
+        } else {
+            await unlink(filePath)
+        }
+    }
+    await rmdir(dirPath)
+}
+
+async function main() {
+    await removeDir(distPath)
+}
+
+main().catch(err => console.error(err))
