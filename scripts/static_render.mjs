0f75edcbb9aa4b604e5101c71775e8e624573007
cbdyzj <cbdyzj@jianzhao.org>
2020-09-15 13:53:06 +0800

    puppeteer

--- scripts/static_render.mjs
+++ scripts/static_render.mjs
@@ -1,5 +1,21 @@
 #!/usr/bin/env node
+import puppeteer from 'puppeteer'
 
-// what to do
+console.time('render')
+const browser = await puppeteer.launch()
 
-console.log('what to do?')
+async function render(url) {
+    const page = await browser.newPage()
+    await page.goto(url, { waitUntil: 'networkidle0' })
+    return page.evaluate(() => document.documentElement.outerHTML)
+}
+
+const baseUrl = 'http://localhost:8080/'
+
+const pageUrls = ['']
+
+// render results
+const results = await Promise.all(pageUrls.map(it => render(baseUrl + it)))
+
+await browser.close()
+console.timeEnd('render')
