2bc64eeae2a947e41b996d84c4479185842a73de
cbdyzj <cbdyzj@jianzhao.org>
2020-09-20 11:52:12 +0800

    Amend

--- web/static/pages/nano/index.jsx
+++ web/static/pages/nano/index.jsx
@@ -2,12 +2,12 @@ import { React } from '/deps.mjs'
 
 import style from './style.module.less'
 
-function Nano() {
+function Index() {
     return (
         <div className={style.container}>
-            <div className={style.title}>Nano</div>
+            <div className={style.title}>Shinonome Research Institute</div>
         </div>
     );
 }
 
-export default Nano
+export default Index
