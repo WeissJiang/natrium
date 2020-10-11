7c26fcbd554844344580b4a992b2e65a63541c80
cbdyzj <cbdyzj@jianzhao.org>
2020-10-11 22:25:53 +0800

    Add ThrottleCache

---
+++ common/src/main/java/nano/support/ThrottleCache.java
@@ -0,0 +1,11 @@
+package nano.support;
+
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+
+/**
+ * Cache updated at a certain pace
+ *
+ * @see ConcurrentMapCache
+ */
+public class ThrottleCache {
+}
