647cd257405af21dc68c91b3b446c3666b78021c
cbdyzj <cbdyzj@jianzhao.org>
2020-12-13 17:12:53 +0800

    try modular

---
+++ common/src/main/java/module-info.java
@@ -0,0 +1,24 @@
+module nano.bot.common {
+    exports nano.support;
+    exports nano.support.mail;
+    exports nano.support.configuration;
+    exports nano.support.validation;
+
+    requires org.jetbrains.annotations;
+
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires spring.aop;
+    requires spring.beans;
+    requires spring.core;
+    requires spring.web;
+    requires spring.webmvc;
+    requires org.apache.tomcat.embed.core;
+    requires org.slf4j;
+    requires spring.context.support;
+    requires jakarta.mail;
+    requires spring.jdbc;
+    requires spring.context;
+    requires java.desktop;
+}
---
+++ web/src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module nano.bot.web {
+    requires spring.context;
+    requires spring.web;
+    requires spring.core;
+    requires json.path;
+    requires org.slf4j;
+    requires nano.bot.common;
+    requires spring.rabbit;
+    requires org.jetbrains.annotations;
+    requires spring.beans;
+    requires spring.amqp;
+    requires java.annotation;
+    requires org.graalvm.sdk;
+    requires spring.webmvc;
+    requires org.apache.tomcat.embed.core;
+    requires uap.java;
+    requires jakarta.mail;
+}
