bca6fb253524dc4f0c4546e4351ff6e625ac9495
cbdyzj <cbdyzj@jianzhao.org>
2020-12-14 20:44:02 +0800

    update common module-info to mark optional dependencies as 'static'

--- common/src/main/java/module-info.java
+++ common/src/main/java/module-info.java
@@ -7,19 +7,19 @@ open module nano.bot.common {
     exports nano.support.jdbc;
 
     requires org.jetbrains.annotations;
-    requires com.fasterxml.jackson.core;
-    requires com.fasterxml.jackson.databind;
-    requires com.fasterxml.jackson.datatype.jsr310;
-    requires spring.aop;
-    requires spring.beans;
-    requires spring.core;
-    requires org.slf4j;
-    requires spring.context.support;
-    requires jakarta.mail;
-    requires spring.context;
-    requires java.desktop;
 
+    requires static java.desktop;
+    requires static jakarta.mail;
     requires static org.apache.tomcat.embed.core;
+    requires static com.fasterxml.jackson.core;
+    requires static com.fasterxml.jackson.databind;
+    requires static com.fasterxml.jackson.datatype.jsr310;
+    requires static org.slf4j;
+    requires static spring.core;
+    requires static spring.beans;
+    requires static spring.context;
+    requires static spring.context.support;
+    requires static spring.aop;
     requires static spring.jdbc;
     requires static spring.web;
     requires static spring.webmvc;
