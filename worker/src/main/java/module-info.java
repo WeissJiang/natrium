70c4da7e16a5a6e6a03637e5affad4c0b7ce4160
cbdyzj <cbdyzj@jianzhao.org>
2021-02-03 22:00:17 +0800

    Add module-info.java

---
+++ worker/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+open module nano.worker {
+    requires nano.common;
+    requires org.jetbrains.annotations;
+    requires org.slf4j;
+    requires jakarta.mail;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.rabbit;
+    requires spring.beans;
+    requires spring.amqp;
+    requires spring.context;
+    requires spring.context.support;
+}
