107e04c0f1f303ce010f244e540173d9baa30fda
cbdyzj <cbdyzj@jianzhao.org>
2021-02-04 23:33:03 +0800

    Add antd

--- client/src/pages/object/Object.jsx
+++ client/src/pages/object/Object.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 
+import 'antd/dist/antd.css'
 import '@/styles/tailwind.css'
+
+import { Button, message, Table, Typography } from 'antd'
+
+const { Title } = Typography
 
 export default function Object(props) {
 
+    function handleClick() {
+        message.info('Clicked')
+    }
+
     return (
-        <div>
-            <span>Object</span>
+        <div className="flex flex-col justify-center items-center py-4">
+            <div>
+                <Title level="3">Object List</Title>
+                <Table />
+            </div>
         </div>
     )
 }
