2f21e09090ab54e77fa3f152acf245288d281a2f
cbdyzj <cbdyzj@jianzhao.org>
2021-05-08 19:05:41 +0800

    Update Alarm

--- client/src/pages/album/Album.jsx
+++ client/src/pages/album/Album.jsx
@@ -2,11 +2,14 @@ import React, { useState } from 'react'
 import saveAs from 'file-saver'
 import { createBook } from './epub.jsx'
 
+import '@/styles/reset.css'
+import '@/styles/utilities.css'
 import styles from './Album.module.css'
 
 export default function Album(props) {
 
     const [loading, setLoading] = useState(false)
+    const [selectedFileName, setSelectedFileName] = useState('')
 
     async function handleSubmit(ev) {
         ev.preventDefault()
@@ -36,13 +39,24 @@ export default function Album(props) {
         }
     }
 
+    function handleFilesSelected(ev) {
+        const files = Array.from(ev.target.files)
+        const filename = files.map(it => it.name).join(',')
+        setSelectedFileName(filename)
+    }
+
     return (
         <div className={styles.container}>
             <form className={styles['form-box']} onSubmit={handleSubmit}>
-                <input disabled={loading} type="file" name="images" required multiple accept=".jpg,.jpeg,.png,.gif" />
                 <input disabled={loading} type="text" name="title" placeholder="Title" />
                 <input disabled={loading} type="text" name="creator" placeholder="Creator" />
                 <input disabled={loading} type="text" name="filename" placeholder="Filename" />
+                <label className="cursor-pointer mt-4">
+                    <span className={styles.selector}>Select Images</span>
+                    <input disabled={loading} type="file" onChange={handleFilesSelected}
+                           name="images" className="hidden" required multiple accept=".jpg,.jpeg,.png,.gif" />
+                </label>
+                <span title={selectedFileName} className={`mt-4 ${styles.tip}`}>{selectedFileName}</span>
                 <button disabled={loading}>Create Album</button>
                 <span>{loading && 'Book creating...'}</span>
             </form>
