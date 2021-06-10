7a91abeb49d05037f938da580a3835f6e36b24ea
cbdyzj <cbdyzj@jianzhao.org>
2021-06-10 14:40:41 +0800

    Amend

--- web/src/main/java/nano/web/telegram/handler/Nano000Handler.java
+++ web/src/main/java/nano/web/telegram/handler/Nano000Handler.java
@@ -4,16 +4,19 @@ import nano.support.Onion;
 import nano.web.nano.model.Bot;
 import nano.web.telegram.BotContext;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Nano000Handler implements Onion.Middleware<BotContext> {
 
+    private static final Logger log = LoggerFactory.getLogger(Nano000Handler.class);
 
     @Override
     public void via(@NotNull BotContext context, Onion.@NotNull Next next) throws Exception {
         if (Bot.NANO_000.equals(context.bot().getName())) {
-            context.replyMessage("Got: " + context.commands());
+            log.info("context.text(): {}", context.text());
         } else {
             next.next();
         }
