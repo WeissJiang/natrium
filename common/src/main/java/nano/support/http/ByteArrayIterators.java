81346de00a4606bf839a652897963a48fb7a4869
cbdyzj <cbdyzj@jianzhao.org>
2021-06-30 01:29:03 +0800

    Fix flag

--- common/src/main/java/nano/support/http/ByteArrayIterators.java
+++ common/src/main/java/nano/support/http/ByteArrayIterators.java
@@ -18,7 +18,7 @@ public abstract class ByteArrayIterators {
 
             @Override
             public boolean hasNext() {
-                return this.drained;
+                return !this.drained;
             }
 
             @Override
