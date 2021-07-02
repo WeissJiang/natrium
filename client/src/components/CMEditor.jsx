f66ffcfde6ed0df02c7f35cdb1df057ffc04158a
cbdyzj <cbdyzj@jianzhao.org>
2021-07-02 21:30:10 +0800

    Use createReactClass

--- client/src/components/CMEditor.jsx
+++ client/src/components/CMEditor.jsx
@@ -1,29 +1,26 @@
-import React, { Component, createRef } from 'react'
+import React, { createRef } from 'react'
 import styled from 'styled-components'
+import createReactClass from 'create-react-class'
 import { EditorView } from '@codemirror/view'
 import { EditorState } from '@codemirror/state'
 
 const Container = styled.div`
-    
+
 `
 
-export default class CMEditor extends Component {
-
-    constructor(props) {
-        super(props);
-        this.containerRef = createRef()
-    }
-
+const CMEditor = createReactClass({
+    containerRef: createRef(),
     componentDidMount() {
         this.editor = new EditorView({
             state: EditorState.create({}),
             parent: this.containerRef.current,
         })
-    }
-
+    },
     render() {
         return (
             <Container ref={this.containerRef}/>
         )
     }
-}
+})
+
+export default CMEditor
