c9b491dad452737e930864994f567894b1798105
cbdyzj <cbdyzj@jianzhao.org>
2021-07-03 14:18:57 +0800

    Use Iterable

--- common/src/main/java/nano/support/http/MultiPartBodyPublisher.java
+++ common/src/main/java/nano/support/http/MultiPartBodyPublisher.java
@@ -1,5 +1,6 @@
 package nano.support.http;
 
+import nano.support.Pair;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.core.io.Resource;
 
@@ -14,71 +15,72 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class MultiPartBodyPublisher {
 
-    private final Map<String, String> stringParts = new HashMap<>();
-    private final Map<String, FilePart> fileParts = new HashMap<>();
+    private final List<Pair<String, String>> stringPartList = new ArrayList<>();
+    private final List<Pair<String, FilePart>> filePartList = new ArrayList<>();
+
     private final String boundary = UUID.randomUUID().toString();
 
     public void addPart(@NotNull String name, @NotNull String value) {
         Objects.requireNonNull(name, "name must be not null");
         Objects.requireNonNull(value, "value must be not null");
-        this.stringParts.put(name, value);
+        this.stringPartList.add(Pair.of(name, value));
     }
 
     public void addPart(@NotNull String name, @NotNull MultiPartBodyPublisher.FilePart filePart) {
         Objects.requireNonNull(name, "name must be not null");
         Objects.requireNonNull(filePart, "filePart must be not null");
-        this.fileParts.put(name, filePart);
+        this.filePartList.add(Pair.of(name, filePart));
     }
 
-    public HttpRequest.BodyPublisher build() {
-        if (this.stringParts.isEmpty() && this.fileParts.isEmpty()) {
+    public @NotNull HttpRequest.BodyPublisher build() {
+        if (this.stringPartList.isEmpty() && this.filePartList.isEmpty()) {
             throw new IllegalStateException("Must have at least one part to build multipart message.");
         }
-        return HttpRequest.BodyPublishers.ofByteArrays(this::getByteIterator);
+        return HttpRequest.BodyPublishers.ofByteArrays(this.getBytes());
     }
 
-    public String getBoundary() {
+    public @NotNull String getBoundary() {
         return this.boundary;
     }
 
-    private @NotNull Iterator<byte[]> getByteIterator() {
-        return ByteArrayIterators.compose(
-                this.getStringPartByteArrayIterator(),
-                this.getFilePartByteArrayIterator(),
-                this.getFinalBoundaryByteArrayIterator()
+    private @NotNull Iterable<byte[]> getBytes() {
+        return ByteArrayIterable.compose(
+                this.getStringPartBytes(),
+                this.getFilePartByteArray(),
+                this.getFinalBoundaryBytes()
         );
     }
 
-    private Iterator<byte[]> getStringPartByteArrayIterator() {
-        return ByteArrayIterators.map(this.stringParts.entrySet().iterator(), entry -> {
-            var name = entry.getKey();
-            var value = entry.getValue();
+    private @NotNull Iterable<byte[]> getStringPartBytes() {
+        return ByteArrayIterable.map(this.stringPartList, entry -> {
+            var name = entry.getLeft();
+            var value = entry.getRight();
             var part = "--" + this.boundary + "\r\n" +
                     "Content-Disposition: form-data; name=\"" + name + "\"\r\n" +
                     "Content-Type: text/plain; charset=UTF-8\r\n\r\n" + value + "\r\n";
-            return ByteArrayIterators.singleton(part.getBytes(UTF_8));
+            return List.of(part.getBytes(UTF_8));
         });
     }
 
-    private Iterator<byte[]> getFilePartByteArrayIterator() {
-        return ByteArrayIterators.map(this.fileParts.entrySet().iterator(), entry -> {
-            var name = entry.getKey();
-            var filePart = entry.getValue();
+    private @NotNull Iterable<byte[]> getFilePartByteArray() {
+        return ByteArrayIterable.map(this.filePartList, entry -> {
+            var name = entry.getLeft();
+            var filePart = entry.getRight();
             var filename = filePart.getFilename();
             var contentType = filePart.getContentType();
             var partHeader = "--" + this.boundary + "\r\n" +
                     "Content-Disposition: form-data; name=\"" + name + "\"; filename=\"" + filename + "\"\r\n" +
                     "Content-Type: " + contentType + "\r\n\r\n";
-            return ByteArrayIterators.compose(
-                    ByteArrayIterators.singleton(partHeader.getBytes(UTF_8)),
-                    ByteArrayIterators.from(filePart.getInputStream()),
-                    ByteArrayIterators.singleton("\r\n".getBytes(UTF_8))
+            return ByteArrayIterable.compose(
+                    List.of(partHeader.getBytes(UTF_8)),
+                    ByteArrayIterable.from(filePart::getInputStream),
+                    List.of("\r\n".getBytes(UTF_8))
             );
         });
     }
 
-    private Iterator<byte[]> getFinalBoundaryByteArrayIterator() {
-        return ByteArrayIterators.singleton(("--" + this.boundary + "--").getBytes(UTF_8));
+    private Iterable<byte[]> getFinalBoundaryBytes() {
+        return List.of(("--" + this.boundary + "--").getBytes(UTF_8));
     }
 
     @FunctionalInterface
---
+++ common/src/test/java/nano/support/http/ByteArrayIteratorsTests.java
@@ -0,0 +1,30 @@
+package nano.support.http;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Stream;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class ByteArrayIteratorsTests {
+
+    @Test
+    public void test() {
+        var strings = Stream.generate(ThreadLocalRandom.current()::nextInt).limit(3).map(String::valueOf).toArray(String[]::new);
+        var bytes = ByteArrayIterable.compose(
+                List.of(strings[0].getBytes(UTF_8)),
+                ByteArrayIterable.from(() -> new ByteArrayInputStream(strings[1].getBytes(UTF_8))),
+                ByteArrayIterable.map(List.of(strings[2]), it -> List.of(it.getBytes(UTF_8)))
+        );
+        var stream = new ByteArrayOutputStream();
+        for (var it : bytes) {
+            stream.writeBytes(it);
+        }
+        Assertions.assertEquals(String.join("", strings), stream.toString(UTF_8));
+    }
+}
